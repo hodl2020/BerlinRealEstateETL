1da6f213c5bbecaa10a6100971cae5773550b2a2
Hodl2020 <37153477+hodl2020@users.noreply.github.com>
2019-04-25 12:32:32 +0200

    AWS config
    Replace your aws  S3 host and redshift db pwd

---
+++ awsconfig.py
@@ -0,0 +1,13 @@
+# Add your aws host,access keys and db pwd below
+
+redshift = {'host': 'yourhost',
+            'user': 'youruser',
+            'pwd': 'yourpwd',
+            'db': 'dbname',
+            'port': 5439
+            }
+
+s3 = { 'key' : 'YourAccessKey',
+          'skey' : 'YourSecretKey',
+          'region' : 'eu-central-1'   
+          }
