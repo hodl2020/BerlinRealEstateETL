dedb95279ba337e7c553dc7d857f2afdd440ed8b
Hodl2020 <37153477+hodl2020@users.noreply.github.com>
2019-05-08 12:01:39 +0200

    added logger and comments

--- s3upload.py
+++ s3upload.py
@@ -1,18 +1,29 @@
 import boto3
 import pathlib
-import os
+import logging
+import fileops
+
     
 
-# define the path, I am using current dir
-currentDirectory = pathlib.Path('.')
+def uploads3():
+        # configure logging
+        logging.basicConfig(filename = 's3upload.log',level=logging.INFO,
+                    format='%(asctime)s %(name)s.%(funcName)s +%(lineno)s: %(levelname)-8s [%(process)d] %(message)s',
+                    )
+        # search for csv files 
+        searchpattern = "*.csv"
+        try:
+            # get latest filename
+            currentfile = fileops.get_latest_file(searchpattern)
+            #upload to s3
+            s3 = boto3.resource('s3')
+            s3.meta.client.upload_file(str(currentfile), 'incoming-eu-bucket-2',  str(currentfile))
+            logging.info('{indent} uploading file: {filename}'.format(indent=3*' ', filename=currentfile))
+            # remove current file from local (you can archive if needed)
+            fileops.remove(currentfile)
+        except Exception:
+            logging.error("Exception while uploading file to S3", exc_info=True)
+            
 
-# search for csv files 
-currentPattern = "*.csv"
-
-for currentFile in currentDirectory.glob(currentPattern):  
-    print(currentFile)
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file(str(currentFile), 'mayur-eu-bucket-1',  str(currentFile))
-    os.remove(currentFile)
-
-
+if __name__ == "__main__":
+    uploads3()
